effc6d60aa1af9e357c76fee07df633e5b1924f4
Samantha Diaz <samidiaz3@gmail.com>
2022-01-15 14:21:09 -0500

    commit tree 799d64ada5275f2d8b4d5cbebcd431e77f6e46d2

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+
+    let element = document.getElementById("text")
+    // element.textContent = "This is really cool!"
+    element.innerHTML = "This is really cool!"
+  });
+// SOLUTION
+// document.addEventListener( "DOMContentLoaded", function () {
+//     updateDOM()
+//   } );
+  
+  
+//   function updateDOM() {
+//     document.getElementById( "text" )
+//       .innerHTML = "This is really cool!";
+//   }
+
+
